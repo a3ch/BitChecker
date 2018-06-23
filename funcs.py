c4ea53aae748c4e16122b1baea46182d16b12948
Maki <cmbyep@gmail.com>
2018-06-23 13:59:13 -0300

    Novas utilidades para algumas funções, e a criação de uma segunda tabela como histórico

--- funcs.py
+++ funcs.py
@@ -15,22 +15,44 @@ def commit_close(func):
     return func_internal
 
 @commit_close
-def db_insert(array):
+def db_insert(array, mode=True):
+    if mode:
+        return """
+        INSERT INTO base(id,name, price_USD, date, time) VALUES('{}', '{}', '{}', DATE('now'), time('now'))
+        """.format(*array)
     return """
-    INSERT INTO base(name, price_USD, date, time) VALUES('{}', '{}', DATE('now'), time('now'))
+    INSERT INTO base_reg(id,price_USD, date, time) VALUES('{}', '{}', DATE('now'), time('now'))
     """.format(*array)
 
+
 @commit_close
-def db_create(name):
+def db_create(name, mode=True):
+    if mode:
+
+        return """
+        CREATE TABLE {}(id INTEGER NOT NULL PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    price_USD REAL NOT NULL,
+                    date TEXT NOT NULL,
+                    time TEXT NOT NULL)
+        """.format(name)
+
     return """
-    CREATE TABLE {}(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
+    CREATE TABLE {}(id INTEGER NOT NULL,
                 price_USD REAL NOT NULL,
                 date TEXT NOT NULL,
-                time TEXT NOT NULL)
+                time TEXT NOT NULL,
+                id_fix INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)
     """.format(name)
 
-# Única func que não recebe o decorador
+@commit_close
+def db_update(price,field):
+    return """
+    UPDATE base SET price_USD='{}', date=DATE('now'), time=TIME('now') WHERE id='{}'
+    """.format(price,field)
+
+# Única func que não recebe o decorado
+# ALTEREI O FETCHALL
 def db_select(data ,field):
     con = sqlite3.connect("base.db")
     cur = con.cursor()
@@ -38,7 +60,9 @@ def db_select(data ,field):
     SELECT * FROM base WHERE {}={}
     """.format(data, field)
     cur.execute(sql)
-    data = cur.fetchall()
+    data = cur.fetchone()
     con.close()
 
-    return data
+    if data == None:
+        return data
+    return data[0]
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 import time
 from pathlib import Path
-from funcs import db_create, db_insert, db_select
+from funcs import db_create, db_insert, db_select, db_update
 
 timeToSleep = 30 # In seconds
 
 if not Path("base.db").is_file():
     db_create("base")
+    db_create("base_reg",False)
 
 try:
     while True:
@@ -15,11 +16,20 @@ try:
 
         listkeys = json["data"].keys()
         for id in listkeys:
-            data = [json["data"][id]["name"], json["data"]
+            data = [id, json["data"][id]["name"], json["data"]
                     [id]["quotes"]["USD"]["price"]]
-            db_insert(data)
-        
+
+            res = str(db_select("id",id))
+            
+            if res in data:
+                db_update(data[2],id)
+            else:
+                db_insert(data)
+
+            db_insert(data[0::2], False)
+
+
         print("Running.")
         time.sleep(timeToSleep)
 except KeyboardInterrupt:
-    print("Terminated")
+    print("\nTerminated")
